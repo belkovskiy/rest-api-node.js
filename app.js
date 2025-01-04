58f950ec865190a0b3733809c425e08c417af5ee
Evgeniy Belkovskiy <ebelkovskiy9@gmail.com>
2025-01-05 00:40:09 +0300

    [feat] Add code for use body-parser and cors

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
